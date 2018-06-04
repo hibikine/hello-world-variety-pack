f5d0582e8c901d7b1fbd7d6379c6074d365621b3
Hibikine Kage <goodsmile314159@gmail.com>
2018-06-04 22:01:15 +0900

    test: add test

--- rails/test/controllers/welcome_controller_test.rb
+++ rails/test/controllers/welcome_controller_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get welcome_index_url
+    get "/"
     assert_response :success
+    assert_select 'h1', "Hello world!"
   end
 
 end
